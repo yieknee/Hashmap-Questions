afc8df6bd0c590b7dc147f58e57bc09bf039e0d5
CheezItMan <chris@adadevelopersacademy.org>
2019-10-07 06:26:43 -0700

    palindrome permutations

---
+++ test/palindrome_permutation_test.rb
@@ -0,0 +1,15 @@
+require_relative "test_helper"
+
+describe "palindrome_permutation?" do
+  it "will work for hello" do
+    expect(palindrome_permutation?("hello")).must_equal false
+  end
+
+  it "will work for 'carrace'" do
+    expect(palindrome_permutation?("carrace")).must_equal true
+  end
+
+  it "will work for emptystring" do
+    expect(palindrome_permutation?("")).must_equal true
+  end
+end
