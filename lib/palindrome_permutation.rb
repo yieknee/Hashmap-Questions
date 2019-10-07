9ea29b0e531eda1c1d9f3eaca1d6941464c5ec2a
CheezItMan <chris@adadevelopersacademy.org>
2019-10-07 06:26:05 -0700

    palindrome_permutation? solution

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,18 @@
 
 def palindrome_permutation?(string)
+  hash = Hash.new(0)
+  letters = string.split("")
 
+  letters.each do |letter|
+    hash[letter] += 1
+  end
+
+  odds = 0
+
+  hash.keys.each do |key|
+    odds += hash[key] % 2 == 0 ? 0 : 1
+    return false if odds > 1
+  end
+
+  return true
 end
