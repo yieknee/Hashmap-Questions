0c3571bd6c032b6e0e442b7df0b48c7dd72b4c10
CheezItMan <chris@adadevelopersacademy.org>
2019-10-07 06:17:33 -0700

    array intersection solution

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,2 +1,16 @@
 def intersection(list1, list2)
+  hash = {}
+  result = []
+
+  list1.each do |element| 
+    hash[element] = true
+  end
+
+  list2.each do |element|
+    if hash[element]
+      result << element
+    end
+  end
+
+  return result
 end
