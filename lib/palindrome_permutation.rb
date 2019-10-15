ee67be7e1dc612353697c70f5312ac372e25f850
CheezItMan <chris@adadevelopersacademy.org>
2019-10-15 14:53:57 -0700

    palindrome permutation not finished

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,18 +1,4 @@
 
 def palindrome_permutation?(string)
-  hash = Hash.new(0)
-  letters = string.split("")
-
-  letters.each do |letter|
-    hash[letter] += 1
-  end
-
-  odds = 0
-
-  hash.keys.each do |key|
-    odds += hash[key] % 2 == 0 ? 0 : 1
-    return false if odds > 1
-  end
-
-  return true
+  raise NotImplementedError, "palindrome_permutation? not implemented"
 end
