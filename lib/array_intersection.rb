b66eaecfdd1e3d51663a3d764dd28e936aec26bd
CheezItMan <chris@adadevelopersacademy.org>
2019-10-15 14:53:25 -0700

    array_intersection not finished

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,16 +1,3 @@
 def intersection(list1, list2)
-  hash = {}
-  result = []
-
-  list1.each do |element| 
-    hash[element] = true
-  end
-
-  list2.each do |element|
-    if hash[element]
-      result << element
-    end
-  end
-
-  return result
+  raise NotImplementedError, "Intersection not implemented"
 end
