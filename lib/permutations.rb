5a94f90ab64bff385599df3394461f65d7577860
CheezItMan <chris@adadevelopersacademy.org>
2019-10-15 14:54:40 -0700

    permutations finished

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,27 +1,4 @@
 
 def permutations?(string1, string2)
-  list1 = string1.split("")
-  list2 = string2.split("")
-
-  hash = {}
-
-  list1.each do |letter|
-    hash[letter] = hash[letter].nil? ? 1 : hash[letter] + 1
-  end
-
-  list2.each do |letter|
-    if hash[letter]
-      hash[letter] -= 1
-    else
-      return false
-    end
-  end
-
-  hash.keys.each do |key|
-    if hash[key] != 0
-      return false
-    end
-  end
-
-  return true
+  raise NotImplementedError, "permutations? not implemented"
 end
