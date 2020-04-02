6da396420fd5dc64ecae5cf913ff0b956a79701b
Yieni <yaynee8d@gmail.com>
2020-04-02 02:13:50 -0700

    refactor array intersectiom

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,31 +1,14 @@
 def intersection(list1, list2)
   hash_table = {}
   intersection_array = []
-  if list1.length < list2.length
-    list1.each do |num|
-      hash_table[num] = true
-    end
-
-    list2.each do |num|
-      intersection_array << num if hash_table[num] != nil
-    end
-  elsif list2.length < list1.length
-    list2.each do |num|
-      hash_table[num] = false
-    end
-
-    list1.each do |num|
-      intersection_array << num if hash_table[num] != nil
-    end
-  else 
-    list1.each do |num|
-      hash_table[num] = false
-    end
-    
-    list2.each do |num|
-      intersection_array << num if hash_table[num] != nil
-    end
+ 
+  list1.each do |num|
+    hash_table[num] = 1
   end
   
+  list2.each do |num|
+    intersection_array << num if hash_table[num]
+  end
+
   return intersection_array
 end
